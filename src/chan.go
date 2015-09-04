bde2273b6c240000e8e38cbb1ef225aed0ca65c1
ypyf <t34@qq.com>
2015-09-05 01:06:03 +0800

    refine chan.go

--- src/chan.go
+++ src/chan.go
@@ -3,25 +3,24 @@ package main
 import "fmt"
 import "time"
 
-var count = 0
-
 func Worker(sem chan int, lock chan bool, id int) {
-    sem <- 1    // down(P原语)
+    sem <- 1    // down(P原语, sem++)
     fmt.Println(time.Now().Format("15:04:05"), id)
     time.Sleep(1 * time.Second) // 睡眠1秒钟
-    <- sem  // up(V原语)
+    <- sem  // up(V原语, sem--)
     
-    count++
-    if count == 10 {
-        lock <- true
-    }
+    lock <- true
 }
 
 func main() {
-    ch := make(chan int, 2) // 带2个int缓冲的Channel(异步Channel，在读完或写满之前都不会阻塞)
+    ch := make(chan int, 2) // 带2个int缓冲的Channel(异步Channel，在读完或写满之前线程都不会阻塞)
     lock := make(chan bool)
     for i := 0; i < 10; i++ {
         go Worker(ch, lock, i)
     }
-    <- lock
+    
+    // 等待所有线程退出
+    for i := 0; i < 10; i++ {
+        <- lock
+    }
 }
