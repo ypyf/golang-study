4b4d9447b060fae1561f7bce5405106f36d31068
ypyf <t34@qq.com>
2015-09-06 11:21:32 +0800

    format code

--- src/tcp_server.go
+++ src/tcp_server.go
@@ -3,16 +3,18 @@ package main
 import "github.com/firstrow/tcp_server"
 
 func main() {
-    server := tcp_server.New("localhost:9999")
+    server := tcp_server.New(":9999")
 
     server.OnNewClient(func(c *tcp_server.Client) {
         // new client connected
         // lets send some message
         c.Send("Hello")
     })
+
     server.OnNewMessage(func(c *tcp_server.Client, message string) {
         // new message received
     })
+
     server.OnClientConnectionClosed(func(c *tcp_server.Client, err error) {
         // connection with client lost
     })
