c99cfeb3eb05580bc37d04dffc92d35c3b548bbd
ypyf <t34@qq.com>
2015-09-07 21:55:06 +0800

    add env.go

---
+++ env.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "os"
+//import "strings"
+
+func main() {
+    for _, e := range os.Environ() {
+        //s := strings.Split(e, "=")
+        fmt.Printf("%s\n", e)
+    }
+}
