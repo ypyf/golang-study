71f5e203dd9f88ab61a93715ed04b7d118a75554
ypyf <t34@qq.com>
2015-09-17 12:24:21 +0800

    update env.go

--- env.go
+++ env.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 import "os"
-
-//import "strings"
+import "strings"
 
 func main() {
 	for _, e := range os.Environ() {
-		//s := strings.Split(e, "=")
-		fmt.Printf("%s\n", e)
+		s := strings.Split(e, "=")
+		fmt.Printf("%s = %s\n", s[0], s[1])
 	}
 }
