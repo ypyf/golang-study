62ac799fea7469bcd781a026bc60d720f3623d89
ypyf <t34@qq.com>
2015-09-22 10:06:26 +0800

    update chan.go

--- chan.go
+++ chan.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 import "time"
 
-func Worker(sem chan int, lock chan<- bool, id int) {
-	sem <- 1 // down(P原语, sem++)
+func Worker(sem chan int, lock chan<- struct{}, id int) {
+	sem <- 1 // down(P原语)
 	fmt.Println(time.Now().Format("15:04:05"), id)
 	time.Sleep(1 * time.Second) // 睡眠1秒钟
-	<-sem                       // up(V原语, sem--)
+	<-sem                       // up(V原语)
 
-	lock <- true
+	lock <- struct{}{}
 }
 
 func main() {
 	ch := make(chan int, 2) // 带2个int缓冲的Channel(异步Channel，在读完或写满之前线程都不会阻塞)
-	lock := make(chan bool)
+	lock := make(chan struct{})
 	for i := 0; i < 10; i++ {
 		go Worker(ch, lock, i)
 	}
