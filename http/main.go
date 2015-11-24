3229fdae82ef90d25ac13d336699e96c41620282
ypyf <t34@qq.com>
2015-11-24 14:53:20 +0800

    Add http/main.go

---
+++ http/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+//"io/ioutil"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("www.baidu.com")
+	if err != nil {
+		log.Fatalf("error %v\n", err)
+	}
+
+	io.Copy(os.Stdout, resp.Body)
+}
