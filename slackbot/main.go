103e8e7fb82b6e19b0d42949c1ea8a7c6d2888fc
ypyf <t34@qq.com>
2015-11-26 08:24:28 +0800

    use Commander interface

--- slackbot/main.go
+++ slackbot/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/nlopes/slack"
+	"os"
 	"regexp"
 	"strings"
-	"os"
-	"github.com/nlopes/slack"
 )
 
 const (
@@ -15,19 +15,52 @@ const (
 	mxTeamID = "T0F8CMAU9"
 )
 
-//func initSego() sego.Segmenter {
-//	// 载入词典
-//	var segmenter sego.Segmenter
-//	segmenter.LoadDictionary("E:/projects/Go/src/github.com/huichen/sego/data/dictionary.txt")
-//	//	segmenter := initSego()
-//	//	segments := segmenter.Segment([]byte(cmd))
-//	//	output := sego.SegmentsToString(segments, false)
-//	//	//输出分词后的成语
-//	//	for _, v := range strings.Fields(output) {
-//	//		fmt.Println(v)
-//	//	}
-//	return segmenter
-//}
+type Message struct {
+	*slack.RTM
+	text    string
+	channel string
+}
+
+func (m *Message) Send(msg string) {
+	m.SendMessage(m.NewOutgoingMessage(msg, m.channel))
+}
+
+func (m *Message) Done() {
+
+}
+
+type Commander interface {
+	Matches(text string) bool
+	Respond(msg *Message)
+	Help() string
+}
+
+type Help struct {
+	sender string // sender id
+}
+
+func (self *Help) Matches(text string) bool {
+	return strings.HasPrefix(text, "help")
+}
+
+func (self *Help) Respond(msg *Message) {
+	tokens := strings.Fields(msg.text)
+	if len(tokens) >= 2 {
+		for _, v := range botCommands {
+			if v.Matches(tokens[1]) {
+				msg.Send(v.Help())
+				break
+			}
+		}
+	} else {
+		msg.Send("当前可用的命令:\nhelp\nhello\n")
+	}
+	msg.Done()
+}
+
+func (self *Help) Help() string {
+	return "你可以尝试输入 help <cmd> 来获得各种命令的帮助信息."
+}
 
 func checkMessage(msg string) (string, bool) {
 	r := regexp.MustCompile("^<@([\\d\\w]+)>:(.*)")
@@ -38,62 +71,58 @@ func checkMessage(msg string) (string, bool) {
 	return s[2], s[1] == botID
 }
 
-//
-// show consul members 显示所有consul节点
-//
-//
-const help = `
-	me -- 显示当前用户信息
-	show all nodes -- 显示 consul 节点信息
-	show node health -- 显示节点健康监控
-`
-func handleCommand(rtm *slack.RTM, dm bool, sender, cmd string) {
-	fmt.Printf("用户 %s 发送指令 %s\n", sender, cmd)
-	tokens := strings.Fields(cmd)
-	var target, channel string
-	if dm {
-		target = ""
-		channel = botDMChannelID
-	} else {
-		target = "<@" + sender + ">: "
-		channel = botChannelID
-	}
+func handleCommand(rtm *slack.RTM, channel, sender, text string) {
+	//fmt.Printf("用户 %s 发送指令 %s\n", sender, text)
 
-	if strings.HasPrefix(tokens[0], "你是谁") || strings.HasPrefix(tokens[0], "你叫什么") {
-		user, _ := rtm.GetUserInfo(botID)
-		fullname := user.Profile.FirstName + " " + user.Profile.LastName
-		rtm.SendMessage(rtm.NewOutgoingMessage(target + "我是 " + fullname, channel))
-		return
-	}
+	//target = "<@" + sender + ">: "
 
-	if strings.HasPrefix(tokens[0], "我是谁") {
-		user, _ := rtm.GetUserInfo(sender)
-		fullname := user.Profile.FirstName + " " + user.Profile.LastName
-		reply := fmt.Sprintf("你是 %s，以下是你的个人资料:\nID: %s\n用户名: %s\nEmail: %s\n电话: %s\n",
-			fullname, user.ID, user.Name, user.Profile.Email, user.Profile.Phone)
-		rtm.SendMessage(rtm.NewOutgoingMessage(target + reply, channel))
-		return
+	for _, v := range botCommands {
+		if v.Matches(text) {
+			v.Respond(&Message{rtm, text, channel})
+			break
+		}
 	}
-
-	switch tokens[0] {
-	case "help":
-		rtm.SendMessage(rtm.NewOutgoingMessage(target + help, channel))
-	default:
-		// echo unknown command
-		rtm.SendMessage(rtm.NewOutgoingMessage(target + cmd, channel))
-	}
-
 	return
 }
 
+type Hello struct {
+	name string
+}
+
+func (w *Hello) Matches(text string) bool {
+	return strings.HasPrefix(text, "hello")
+}
+
+func (w *Hello) Respond(msg *Message) {
+	msg.Send(fmt.Sprintf("你好，我是机器人 %s，有什么需要我帮助的吗？\n", w.name))
+	msg.Done()
+}
+
+func (w *Hello) Help() string {
+	return "打招呼."
+}
+
+type BotCommands []Commander
+var botCommands BotCommands
+
 func handleMessage(rtm *slack.RTM) {
+	// 注册命令处理器
+	user, err := rtm.GetUserInfo(botID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fullname := user.Profile.FirstName + " " + user.Profile.LastName
+	botCommands = append(botCommands, &Help{})
+	botCommands = append(botCommands, &Hello{name: fullname})
+
 	for {
 		select {
 		case msg := <-rtm.IncomingEvents:
 			fmt.Print("Event Received: ")
 			switch evt := msg.Data.(type) {
 			case *slack.HelloEvent:
-			// Ingore hello
+			// Ignore hello
 			case *slack.ConnectedEvent:
 				fmt.Println("Info:", evt.Info)
 				fmt.Println("Connection counter:", evt.ConnectionCount)
@@ -101,16 +130,16 @@ func handleMessage(rtm *slack.RTM) {
 			case *slack.MessageEvent:
 				fmt.Printf("Message: %v\n", evt)
 				if evt.Channel == botChannelID && evt.Team == mxTeamID {
-					cmd, sendToMXBot := checkMessage(evt.Text)
-					if sendToMXBot && len(cmd) > 0 {
+					text, sendToMXBot := checkMessage(evt.Text)
+					if sendToMXBot && len(text) > 0 {
 						//fmt.Printf("接收到机器人指令： %v", evt)
-						go handleCommand(rtm, false, evt.User, cmd)
+						go handleCommand(rtm, evt.Channel, evt.User, strings.TrimSpace(text))
 					}
 				} else if evt.Channel == botDMChannelID && evt.Team == mxTeamID {
-					go handleCommand(rtm, true, evt.User, evt.Text)
+					go handleCommand(rtm, evt.Channel, evt.User, strings.TrimSpace(evt.Text))
 				}
 			case *slack.ChannelJoinedEvent:
-			//
+			// Ignore
 			case *slack.PresenceChangeEvent:
 				fmt.Printf("Presence Change: %v\n", evt)
 			case *slack.LatencyReport:
