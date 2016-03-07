af0cc55f8df5858961f07f5df0acc8ff999cadab
ypyf <t34@qq.com>
2016-03-07 16:47:12 +0800

    update env.go

--- env.go
+++ env.go
@@ -5,6 +5,11 @@ import "os"
 import "strings"
 
 func main() {
+	// 获取单个变量
+	foo := os.Getenv("FOO")
+	fmt.Println("FOO:", foo)
+
+	// 获取全部变量
 	for _, e := range os.Environ() {
 		s := strings.Split(e, "=")
 		fmt.Printf("%s = %s\n", s[0], s[1])
