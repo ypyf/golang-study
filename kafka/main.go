e8935c3564c3770ee871f78775393c93afe23a9f
ypyf <t34@qq.com>
2016-04-15 14:59:31 +0800

    update kafka/main.go

--- kafka/main.go
+++ kafka/main.go
@@ -6,7 +6,16 @@ import (
 	"os/signal"
 
 	"github.com/Shopify/sarama"
+	"gopkg.in/redis.v3"
 )
+
+func openRedis() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "10.10.149.62:6379",
+		Password: "",
+		DB:       0, // use default DB
+	})
+}
 
 func eat() {
 	consumer, err := sarama.NewConsumer([]string{"10.10.40.151:9092"}, nil)
@@ -35,13 +44,16 @@ func eat() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 
+	redis := openRedis()
 	consumed := 0
 ConsumerLoop:
 	for {
 		select {
 		case msg := <-partitionConsumer.Messages():
 			log.Printf("Consumed message offset %d\n", msg.Offset)
-			log.Printf("Consumed message value %s\n", string(msg.Value))
+			redis.SAdd("atime_ap", string(msg.Value))
+			//log.Printf("Consumed message offset %d\n", msg.Offset)
+			//log.Printf("Consumed message value %s\n", string(msg.Value))
 			consumed++
 		case <-signals:
 			break ConsumerLoop
