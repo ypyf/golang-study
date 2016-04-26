f14064a7907b9abfcb6626f1868d1d47726fcd8e
ypyf <t34@qq.com>
2016-04-26 21:15:57 +0800

    refine code

--- rot13Reader.go
+++ rot13Reader.go
@@ -20,23 +20,24 @@ type rot13Reader struct {
 // 实现 Read([]byte) (int, error) 接口
 func (rot rot13Reader) Read(b []byte) (int, error) {
 	n, err := rot.Reader.Read(b)
-	if err == nil {
-		for i := 0; i < n; i++ {
-			if unicode.IsUpper(rune(b[i])) {
-				j := int8(b[i] - 'A')
-				j -= 13
-				if j < 0 {
-					j = 26 + j
-				}
-				b[i] = t0[j]
-			} else if unicode.IsLower(rune(b[i])) {
-				j := int8(b[i] - 'a')
-				j -= 13
-				if j < 0 {
-					j = 26 + j
-				}
-				b[i] = t1[j]
+	if err != nil {
+		return n, err
+	}
+	for i := 0; i < n; i++ {
+		if unicode.IsUpper(rune(b[i])) {
+			j := int8(b[i] - 'A')
+			j -= 13
+			if j < 0 {
+				j = 26 + j
 			}
+			b[i] = t0[j]
+		} else if unicode.IsLower(rune(b[i])) {
+			j := int8(b[i] - 'a')
+			j -= 13
+			if j < 0 {
+				j = 26 + j
+			}
+			b[i] = t1[j]
 		}
 	}
 	return n, err
