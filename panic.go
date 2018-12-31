05ab31bb73f61d238f4b4ceba4fc8eabb6007641
yf <t34@qq.com>
2019-01-01 03:27:17 +0800

    Update rename some functions

--- panic.go
+++ panic.go
@@ -5,24 +5,23 @@ import (
 )
 
 func main() {
-	f()
-	fmt.Println("Returned normally from f.")
+	shouldRecover()
+	fmt.Println("Returned normally from shouldRecover().")
 }
 
-func f() {
+func shouldRecover() {
 	// 必须放在可能发生panic的代码之前，否则在panic发生时并不会执行到defer中的代码
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println("Recovered in f", r)
-			fmt.Println("Recovered in f", r)
+			fmt.Printf("Recovered in shouldRecover(): %s\n", r)
 		}
 	}()
 	fmt.Println("Calling g.")
-	g()
-	fmt.Println("Returned normally from g.")
+	mayPanic()
+	fmt.Println("Returned normally from mayPanic().")
 
 }
 
-func g() {
+func mayPanic() {
 	panic("ERROR")
 }
